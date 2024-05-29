3caf7ce347ef94b5120971edea34312b574c8bbe
Lok <lok@pg95.com>
2024-05-30 00:18:09 +0100

    fixing post

--- routes/api.js
+++ routes/api.js
@@ -7,41 +7,155 @@
 */
 
 'use strict';
+require('dotenv').config();
 
 module.exports = function (app) {
 
-  app.route('/api/books')
-    .get(function (req, res){
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
-    })
-    
-    .post(function (req, res){
-      let title = req.body.title;
-      //response will contain new book object including atleast _id and title
-    })
-    
-    .delete(function(req, res){
-      //if successful response will be 'complete delete successful'
-    });
+  const mongoose = require('mongoose')
+  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(()=>{
+    console.log("Connected to MongoDB")
+  })
+  .catch((e)=>{
+    console.log("Cannot Connected to MongoDB")
+    console.log(e)
+  });
 
+  const bookSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: true
+    },
+    commentcount: {
+      type: Number,
+      default: 0
+    }
+  })
+
+  const commentSchema = new mongoose.Schema({
+    bookid: {
+      type: String,
+      required: true
+    },
+    comment: {
+      type: String,
+      required: true
+    }
+  })
+
+  app.route('/api/books')
+  .get(async function (req, res){
+    //response will be array of book objects
+    //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+    let Book = mongoose.model('Book', bookSchema)
+    let id = req.query._id
+
+    try {
+        await Book.find({})
+        .then(docs => {
+          res.json(docs)
+        })
+     } catch(e) { 
+      console.log(e)
+      res.json({result:'no book exists'});
+     }
+  })
+  .post(async function (req, res){
+    let title = req.body.title;
+    let Book = mongoose.model('Book', bookSchema)
+    //response will contain new book object including atleast _id and title
+
+    if (!title) res.send('missing required field title') 
+    let newBook = new Book({title: title})
+
+    await newBook.save()
+    .then(
+      doc => {
+      res.json(doc);
+      },
+      error => {
+      console.log(e);
+      res.json({error:e});
+      }
+    )
+
+  })
+  .delete(function(req, res){
+    //if successful response will be 'complete delete successful'
+  });
 
 
   app.route('/api/books/:id')
-    .get(function (req, res){
+    .get(async function (req, res){
       let bookid = req.params.id;
+      let Book = mongoose.model('Book', bookSchema)
+      let Comment = mongoose.model('Comment', commentSchema)
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      try{
+          await Book.findById({_id: bookid})
+          .then(async doc => {
+            let comments = [];
+            await Comment.find({bookid:bookid}).then(
+              docs => {
+                docs.map((comment) => comments.push(comment));
+              }
+            )
+            res.json({_id:bookid, title:doc.title, comments: comments})
+          })
+      } catch(e) {
+          console.log(e)
+          res.send('no book exists')
+      }
     })
-    
-    .post(function(req, res){
+    // add comment for a book
+    .post(async function(req, res){
       let bookid = req.params.id;
+      console.log('bookid = ' + bookid )
       let comment = req.body.comment;
+      let Book = mongoose.model('Book', bookSchema)
+      let Comment = mongoose.model('Comment', commentSchema)
       //json res format same as .get
+
+      if (!comment) {
+        console.log('missing comment')
+        res.send('missing required field comment')
+      }
+      try {
+        await Book.findById(bookid)
+        .then( async book => {
+          if (book) {
+            let commentCnt = 0
+            //////// Save new comment ///////
+            let newComment = new Comment({bookid:{bookid}, comment: comment});
+            await newComment.save()
+
+            //////// Find all comments ///////
+            let comments = []
+            await Comment.find({bookid:bookid}).then(
+              docs => {
+                docs.map((comment) => comments.push(comment));
+                commentCnt = docs.length
+              }
+            )
+
+            ///////// Update count for the book /////////
+            book.commentCnt = commentCnt
+            await book.save()
+
+            res.json({_id:bookid, title:doc.title, comments: comments})
+
+          } else {
+            res.send('no book exists')
+          }
+        })
+      } catch(e) {
+        console.log(e)
+        res.send('cannot create comment')
+      }
+
     })
-    
     .delete(function(req, res){
       let bookid = req.params.id;
       //if successful response will be 'delete successful'
     });
-  
 };
